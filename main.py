ca5004cb933a8ee632a81c469e8eb5421e695a14
EmSk6442 <92519693+EmSk6442@users.noreply.github.com>
2023-11-06 12:15:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import functions as func
-import math
 
 # read file
-nrows = 500
-df = func.csv_read_FA(FA_20200922T000000UTC, nrows)
+nrows = 100000
+file = "FA_20200922T000000UTC.csv"
+df = func.csv_read_FA(file, nrows)
 
 # unique cows
 u = func.unique_cows(df)
 print(len(u))
+print(len(df))
+
+# sort out cows
+df = func.cows_above_yline(df)
+
+# unique cows
+u = func.unique_cows(df)
+print(len(u))
+print(len(df))
+
+
+## after milking
+
+# for each bed check which cows lay in th bed in order
+
+# identify beds
+
+# init time when milking
+
+# cows in bed, store cow tag id and time t_0 and t_1 in bed
+
+# visualize
