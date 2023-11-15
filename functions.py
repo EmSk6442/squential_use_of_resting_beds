39cd65a1c2eb41cb7e9beeb95e13a638ab31fc18
JOJJO98 <92676383+JOJJO98@users.noreply.github.com>
2023-11-15 10:20:25 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -47,7 +47,7 @@ def cows_above_yline_right_left(df, barn_filename):
     y_line = 2595
 
     #Mean above y-line
-    mean_cow = df.groupby('tag_id').mean()
+    mean_cow = df.groupby('tag_id')[['x', 'y']].mean()
     df_keep = mean_cow[mean_cow['y'] >= y_line].index
     df = df[df["tag_id"].isin(df_keep)]
     
@@ -208,3 +208,142 @@ def plot_time(df, t1, t2):
 ####################################
 ### Animations                  ####
 ####################################
+pause = False
+
+def animate_cows(df, cowID_1, cowID_2, barn_filename, save_path='n'):
+
+    cow_1 = df.loc[df['tag_id'] == cowID_1]
+
+    cow_2 = df.loc[df['tag_id'] == cowID_2]
+
+    x1, y1, z1 = positions(cow_1)
+    x2, y2, z2 = positions(cow_2)
+
+    time1 = list(cow_1['time'])
+    time2 = list(cow_2['time'])
+
+    timestrings1= []
+    timestrings2 = []
+
+    for timestamp1 in time1:
+        timestrings1.append(datetime.date.fromtimestamp((timestamp1/1000)-7200))
+
+    for timestamp2 in time2:
+        timestrings2.append(datetime.date.fromtimestamp((timestamp2 / 1000) - 7200))
+
+    f, ax1 = plot_barn(barn_filename)
+
+
+    ax1.change_geometry(2, 1, 1)
+    ax2 = f.add_subplot(212)
+
+    plt.tight_layout()
+
+    pos1 = ax1.get_position().bounds
+    pos2 = ax2.get_position().bounds
+
+    new_pos1 = [pos1[0], 0.25, pos1[2], 0.7]
+    new_pos2 = [pos2[0], pos2[1], pos2[2], 0.1]
+
+    ax1.set_position(new_pos1)
+    ax2.set_position(new_pos2)
+
+    xdata1, ydata1 = [], []
+    ln1, = ax1.plot([], [], '-')
+    xdata2, ydata2 = [], []
+    ln2, = ax1.plot([], [], '-')
+
+    d1, = ax1.plot([], [], 'co', label='Cow '+ str(cowID_1))
+    d2, = ax1.plot([], [], 'yo', label='Cow '+ str(cowID_2))
+
+    ax1.legend(loc='upper left')
+
+    dist, time = [], []
+    dist_plot, = ax2.plot([], [], 'r-')
+
+    def run_animation():
+        ani_running = True
+        i = 0
+        j = 0
+        def onClick(event): #If the window is clicked, the gif pauses
+            nonlocal ani_running
+            if ani_running:
+                ani.event_source.stop()
+                ani_running = False
+            else:
+                ani.event_source.start()
+                ani_running = True
+
+        def init():
+            ax1.set_xlim(0, 3340)
+            ax1.set_ylim(0, 8738)
+            ax2.set_xlim(timestrings1[0], timestrings1[len(timestrings1)-1])
+
+            ax2.set_ylim(0, 10000)
+            date1 = timestrings1[0]
+            date2 = timestrings1[len(timestrings1)-1]
+            ax1.set_title("Plot of two cows between " + date1.strftime("%d %b %Y %H:%M") + " - " +
+                          date2.strftime("%d %b %Y %H:%M"), fontsize=8)
+            ax2.set_ylabel('Distance(cm)')
+            ax2.set_xlabel('Time of day')
+
+            return ln1, ln2, d1, d2, dist_plot
+
+        def update(frame):
+            nonlocal i
+            nonlocal j
+            if not pause:
+                if time1[i] <= time2[j]:
+                    if i == len(time1) - 1:  # if at end of times_1
+                        j = j + 1
+                        xdata2.append(x2[j])  # new distance
+                        ydata2.append(y2[j])
+                        xdata1.append(x1[i])  # new distance
+                        ydata1.append(y1[i])
+                    else:
+                        i = i + 1
+                        xdata1.append(x1[i])  # new distance
+                        ydata1.append(y1[i])
+                        xdata2.append(x2[j])  # new distance
+                        ydata2.append(y2[j])
+                else:
+                    if j == len(time2) - 1:  # if at end of times_2
+                        i = i + 1
+                        xdata1.append(x1[i])  # new distance
+                        ydata1.append(y1[i])
+                        xdata2.append(x2[j])  # new distance
+                        ydata2.append(y2[j])
+                    else:
+                        j = j + 1
+                        xdata2.append(x2[j])  # new distance
+                        ydata2.append(y2[j])
+                        xdata1.append(x1[i])  # new distance
+                        ydata1.append(y1[i])
+
+                ln1.set_data(xdata1, ydata1) #Uppdate the plot with the data
+                d1.set_data(x1[i], y1[i])
+                ln2.set_data(xdata2, ydata2)
+                d2.set_data(x2[j], y2[j])
+                dist.append(math.sqrt(math.pow(x1[i]-x2[j], 2) + math.pow(y1[i]-y2[j], 2)))
+
+                if time1[i]<time2[j]: #Uppdate the correct time
+                    time.append((timestrings1[i]))
+                else:
+                    time.append((timestrings2[j]))
+
+
+                dist_plot.set_data(time, dist)
+
+            return ln1, ln2, d1, d2, dist_plot
+
+        f.canvas.mpl_connect('button_press_event', onClick)
+        ani = FuncAnimation(f, update, frames=len(time1)+len(time2)-2, init_func=init, blit=True, interval=1, repeat=False) #Main animationfunction
+        if save_path != 'n': #If a filename is given, the gif is saved
+            try:
+                ani.save(save_path)
+            except:
+                print('Wrong filepath')
+
+        plt.show()
+
+    run_animation()
